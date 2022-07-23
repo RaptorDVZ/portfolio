ae68a3d2c484af39547c1e29e3c10d9376ca4945
RaptorDVZ <sdivyanshu105@gmail.com>
2022-07-24 05:27:09 +0530

    First

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.get('/', (req,res)=>{
+    res.sendFile(__dirname+"/index.html");
+})
+
+
+app.get('/div.html', (req,res)=>{
+    res.sendFile(__dirname+"/div.html");
+})
+
+
+app.listen(3000, ()=>{
+    console.log(`The server has started at port ${port}`);
+})
